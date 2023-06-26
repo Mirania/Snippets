50cec33b9e05bf59ffb3d9d21c137754b4901055
Carlos <35841895+Mirania@users.noreply.github.com>
2023-06-26 18:28:53 +0100

    Make scraper async

--- TS - Discord Channel Scraper/src/scraper.ts
+++ TS - Discord Channel Scraper/src/scraper.ts
@@ -22,20 +22,24 @@ client.login(process.argv[2]); // for example: asjkhfhd384jf89938j498._tm3984
 
 // based on https://stackoverflow.com/a/71620968
 // note that this starts at the newest message and "scrolls up" to the oldest message.
-async function processAllMessages(action: (message: Message<true>) => void) {
-    const guild = await client.guilds.fetch(process.argv[3]); // for example: 012930821409823908490
-    const channel = await guild.channels.fetch(process.argv[4]) as TextChannel; // for example: 012930821409823908490
+async function processAllMessages(action: (message: Message<true>) => Promise<void>) {
+    const guild = await client.guilds.fetch(vars.guild); // for example: 012930821409823908490
+    const channel = await guild.channels.fetch(vars.channel) as TextChannel; // for example: 012930821409823908490
 
     // Create message pointer
     let message = await channel.messages
         .fetch({ limit: 1 })
-        .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
+        .then(async messagePage => {
+            const pointer = (messagePage.size === 1 ? messagePage.at(0) : null);
+            if (pointer) await action(pointer);
+            return pointer;
+        });
 
     while (message) {
         await channel.messages
             .fetch({ limit: 100, before: message.id })
-            .then(messagePage => {
-                messagePage.forEach(msg => action(msg));
+            .then(async messagePage => {
+                await Promise.all(messagePage.map(msg => action(msg)));
 
                 // Update our message pointer to be the last message on the page of messages
                 message = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
